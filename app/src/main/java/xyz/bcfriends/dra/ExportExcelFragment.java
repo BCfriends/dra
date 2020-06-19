dc3b27957e3bd13ad6e54e4695353590ed297cd5
Mikeds <darponge@gmail.com>
2020-06-19 22:03:05 +0900

    Add Excel

--- app/src/main/java/xyz/bcfriends/dra/ExportExcelFragment.java
+++ app/src/main/java/xyz/bcfriends/dra/ExportExcelFragment.java
@@ -1,14 +1,22 @@
 package xyz.bcfriends.dra;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
@@ -24,10 +32,14 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Objects;
 
-public class ExportExcelFragment extends Fragment {
+import xyz.bcfriends.dra.util.DBHelper;
+
+public class ExportExcelFragment extends Fragment implements DBHelper.Executor {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,45 +53,86 @@ public class ExportExcelFragment extends Fragment {
 
         String safeName = WorkbookUtil.createSafeSheetName("통계");
         CreationHelper createHelper = wb.getCreationHelper();
+        FirestoreHelper firestoreHelper = new FirestoreHelper(this);
 
         Sheet statSheet = wb.createSheet(safeName);
 
-        int rowNum = 0;
-        int dateCol = 0;
-        boolean depressFlag = true;
+        CheckBox exportMemo = v.findViewById(R.id.export_memo);
+        boolean noteFlag = exportMemo.isChecked();
 
-        Row row = statSheet.createRow(rowNum);
+        firestoreHelper.readDataAll(
+                firestoreHelper.getDatabase().collection("users")
+                        .document(Objects.requireNonNull(firestoreHelper.getUser().getUid()))
+                        .collection("Records"),
+                new DBHelper.QueryCallback() {
+                    @Override
+                    public void onCallback(@Nullable QuerySnapshot data) {
+                        if (data != null) {
+                            for (QueryDocumentSnapshot document : data) {
+                                Row row;
+                                if (statSheet.getLastRowNum() == -1) {
+                                    row = statSheet.createRow(0);
 
-        Cell cell = row.createCell(0);
-        cell.setCellValue(new Date());
+                                    Cell cell = row.createCell(1);
+                                    cell.setCellValue("일자");
+                                    cell = row.createCell(2);
+                                    cell.setCellValue("정도");
+                                    if (noteFlag) {
+                                        cell = row.createCell(3);
+                                        cell.setCellValue("메모");
+                                    }
+                                } else {
+                                    row = statSheet.createRow(statSheet.getLastRowNum() + 1);
+                                }
 
-        CellStyle cellStyle = wb.createCellStyle();
+                                // Date
+                                Cell cell = row.createCell(1);
+                                cell.setCellValue(LocalDate.parse(document.getId()));
+                                CellStyle cellStyle = wb.createCellStyle();
+                                cellStyle.setDataFormat(
+                                        createHelper.createDataFormat().getFormat("yyyy-mm-dd"));
+                                cell.setCellStyle(cellStyle);
 
-        cellStyle.setDataFormat(
-                createHelper.createDataFormat().getFormat("yyyy/mm/dd"));
+                                // DepressStatus
+                                cell = row.createCell(2);
+                                cell.setCellValue(Objects.requireNonNull(document.getData().get("depressStatus")).toString());
 
-        cell = row.createCell(1);
-        cell.setCellValue(new Date());
-        cell.setCellStyle(cellStyle);
+                                // Note
+                                if (noteFlag) {
+                                    cell = row.createCell(3);
+                                    cell.setCellValue(Objects.requireNonNull(document.getData().get("note")).toString());
+                                }
+                            }
+                        }
+                    }
+                });
 
+        File xls = new File(requireActivity().getApplicationContext().getFilesDir() + "/" + safeName + ".xlsx");
 
-        cell = row.createCell(2);
-        cell.setCellValue(Calendar.getInstance());
-        cell.setCellStyle(cellStyle);
-
-
-        String filename = "workbook.xlsx";
-        File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
-        File xls = new File(dir, filename);
-
-        try (OutputStream fileOut = new FileOutputStream(xls)) {
+        try {
+//            xls = File.createTempFile("통계", ".xlsx", requireActivity().getExternalCacheDir());
+            OutputStream fileOut = new FileOutputStream(xls);
             wb.write(fileOut);
+
+            Uri uri = Uri.parse("mailto:darponge@gmail.com");
+            Intent it = new Intent(Intent.ACTION_SENDTO, uri);
+            it.putExtra(Intent.EXTRA_SUBJECT, "excel file email test");
+            it.putExtra(Intent.EXTRA_STREAM, Uri.parse("content://" + xls));
+            startActivityForResult(it, 200);
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        Toast.makeText(requireContext().getApplicationContext(), xls.getAbsolutePath() + "에 저장되었습니다.", Toast.LENGTH_SHORT).show();
+
         return v;
+    }
+
+    @Override
+    public void showResult(String message) {
+
     }
 }
