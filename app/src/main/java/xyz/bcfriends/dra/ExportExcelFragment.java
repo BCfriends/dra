30479697d023679bee8ed480468100515791c09a
Mikeds <darponge@gmail.com>
2020-06-20 17:40:14 +0900

    Add Button OnclickListener

--- app/src/main/java/xyz/bcfriends/dra/ExportExcelFragment.java
+++ app/src/main/java/xyz/bcfriends/dra/ExportExcelFragment.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
@@ -49,85 +50,88 @@ public class ExportExcelFragment extends Fragment implements DBHelper.Executor {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_export_excel, container, false);
-        Workbook wb = new XSSFWorkbook();
 
-        String safeName = WorkbookUtil.createSafeSheetName("통계");
-        CreationHelper createHelper = wb.getCreationHelper();
-        FirestoreHelper firestoreHelper = new FirestoreHelper(this);
+        Button exportButton = v.findViewById(R.id.export_button);
+        exportButton.setOnClickListener(v1 -> {
+            Workbook wb = new XSSFWorkbook();
 
-        Sheet statSheet = wb.createSheet(safeName);
+            String safeName = WorkbookUtil.createSafeSheetName("통계");
+            CreationHelper createHelper = wb.getCreationHelper();
+            FirestoreHelper firestoreHelper = new FirestoreHelper(this);
 
-        CheckBox exportMemo = v.findViewById(R.id.export_memo);
-        boolean noteFlag = exportMemo.isChecked();
+            Sheet statSheet = wb.createSheet(safeName);
 
-        firestoreHelper.readDataAll(
-                firestoreHelper.getDatabase().collection("users")
-                        .document(Objects.requireNonNull(firestoreHelper.getUser().getUid()))
-                        .collection("Records"),
-                new DBHelper.QueryCallback() {
-                    @Override
-                    public void onCallback(@Nullable QuerySnapshot data) {
-                        if (data != null) {
-                            for (QueryDocumentSnapshot document : data) {
-                                Row row;
-                                if (statSheet.getLastRowNum() == -1) {
-                                    row = statSheet.createRow(0);
+            CheckBox exportMemo = v.findViewById(R.id.export_memo);
+            boolean noteFlag = exportMemo.isChecked();
 
+            firestoreHelper.readDataAll(
+                    firestoreHelper.getDatabase().collection("users")
+                            .document(Objects.requireNonNull(firestoreHelper.getUser().getUid()))
+                            .collection("Records"),
+                    new DBHelper.QueryCallback() {
+                        @Override
+                        public void onCallback(@Nullable QuerySnapshot data) {
+                            if (data != null) {
+                                for (QueryDocumentSnapshot document : data) {
+                                    Row row;
+                                    if (statSheet.getLastRowNum() == -1) {
+                                        row = statSheet.createRow(0);
+
+                                        Cell cell = row.createCell(1);
+                                        cell.setCellValue("일자");
+                                        cell = row.createCell(2);
+                                        cell.setCellValue("정도");
+                                        if (noteFlag) {
+                                            cell = row.createCell(3);
+                                            cell.setCellValue("메모");
+                                        }
+                                    } else {
+                                        row = statSheet.createRow(statSheet.getLastRowNum() + 1);
+                                    }
+
+                                    // Date
                                     Cell cell = row.createCell(1);
-                                    cell.setCellValue("일자");
+                                    cell.setCellValue(LocalDate.parse(document.getId()));
+                                    CellStyle cellStyle = wb.createCellStyle();
+                                    cellStyle.setDataFormat(
+                                            createHelper.createDataFormat().getFormat("yyyy-mm-dd"));
+                                    cell.setCellStyle(cellStyle);
+
+                                    // DepressStatus
                                     cell = row.createCell(2);
-                                    cell.setCellValue("정도");
+                                    cell.setCellValue(Objects.requireNonNull(document.getData().get("depressStatus")).toString());
+
+                                    // Note
                                     if (noteFlag) {
                                         cell = row.createCell(3);
-                                        cell.setCellValue("메모");
+                                        cell.setCellValue(Objects.requireNonNull(document.getData().get("note")).toString());
                                     }
-                                } else {
-                                    row = statSheet.createRow(statSheet.getLastRowNum() + 1);
-                                }
-
-                                // Date
-                                Cell cell = row.createCell(1);
-                                cell.setCellValue(LocalDate.parse(document.getId()));
-                                CellStyle cellStyle = wb.createCellStyle();
-                                cellStyle.setDataFormat(
-                                        createHelper.createDataFormat().getFormat("yyyy-mm-dd"));
-                                cell.setCellStyle(cellStyle);
-
-                                // DepressStatus
-                                cell = row.createCell(2);
-                                cell.setCellValue(Objects.requireNonNull(document.getData().get("depressStatus")).toString());
-
-                                // Note
-                                if (noteFlag) {
-                                    cell = row.createCell(3);
-                                    cell.setCellValue(Objects.requireNonNull(document.getData().get("note")).toString());
                                 }
                             }
                         }
-                    }
-                });
+                    });
 
-        File xls = new File(requireActivity().getApplicationContext().getFilesDir() + "/" + safeName + ".xlsx");
+            File xls = new File(requireActivity().getApplicationContext().getFilesDir() + "/" + safeName + ".xlsx");
 
-        try {
+            try {
 //            xls = File.createTempFile("통계", ".xlsx", requireActivity().getExternalCacheDir());
-            OutputStream fileOut = new FileOutputStream(xls);
-            wb.write(fileOut);
+                OutputStream fileOut = new FileOutputStream(xls);
+                wb.write(fileOut);
 
-            Uri uri = Uri.parse("mailto:darponge@gmail.com");
-            Intent it = new Intent(Intent.ACTION_SENDTO, uri);
-            it.putExtra(Intent.EXTRA_SUBJECT, "excel file email test");
-            it.putExtra(Intent.EXTRA_STREAM, Uri.parse("content://" + xls));
-            startActivityForResult(it, 200);
+                Uri uri = Uri.parse("mailto:darponge@gmail.com");
+                Intent it = new Intent(Intent.ACTION_SENDTO, uri);
+                it.putExtra(Intent.EXTRA_SUBJECT, "excel file email test");
+                it.putExtra(Intent.EXTRA_STREAM, Uri.parse("content://" + xls));
+                startActivityForResult(it, 200);
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-        Toast.makeText(requireContext().getApplicationContext(), xls.getAbsolutePath() + "에 저장되었습니다.", Toast.LENGTH_SHORT).show();
-
+            Toast.makeText(requireContext().getApplicationContext(), xls.getAbsolutePath() + "에 저장되었습니다.", Toast.LENGTH_SHORT).show();
+        });
         return v;
     }
 
